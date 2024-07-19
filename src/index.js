c09d77ada1bbcf237e0f41c41e46f728c6e8ac06
Franco Colnaghi <colnaghi.fr@gmail.com>
2024-07-19 18:23:02 -0300

    primera parte de la 2da Entrega

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import express from 'express'
 import producsRoute from './routes/products.router.js'
 import cartsRoute from './routes/carts.router.js'
+import homeRoute from './routes/home.router.js'
+import realTimeProducts from './routes/realtimeproducts.router.js'
 import { __dirname } from './utils.js'
+import handlebars from 'express-handlebars'
+import {Server} from 'socket.io'
+import ProductsManager from './class/productManager.js'
 
 const app = express()
+const productManager = new ProductsManager(__dirname + '/bbdd/products.json');
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
 
 // MIDDLEWARES
-app.use(express.json()) //para indicar que podemos aceptar .JSON x Body
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname + '/public'))
 app.use('/api/products', producsRoute)
 app.use('/api/carts', cartsRoute)
+app.use('/home', homeRoute)
+app.use('/realtimeproducts', realTimeProducts)
 
 // ESCHUCHA
-app.listen(8080,()=>{
+const httpServer = app.listen(8080,()=>{
     console.log("Servidor Iniciado")
+})
+
+export const socketServer = new Server(httpServer)
+
+socketServer.on('connection', async (socket)=>{
+    console.log(`Nuev.Disp.Conect. ID: ${socket.id}`)
+    const productsList = await productManager.getAllProducts()
+    socket.emit('home', productsList)
+    socket.emit('realtime', productsList)
+    socket.on('nuevo-producto', async(producto)=>{
+        await productManager.addProduct(producto)
+        socket.emit('realtime', productsList)
+    })
 })
